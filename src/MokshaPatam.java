7536475c88fe1cf49927d1634bd995c12417148f
kpi <kieran.pichai@menloschool.org>
2024-09-05 23:01:02 -0700

    added some primative code

--- src/MokshaPatam.java
+++ src/MokshaPatam.java
@@ -18,48 +18,73 @@ public class MokshaPatam {
      *  to reach the final square on a board with the given size, ladders, and snakes.
      */
     public static int fewestMoves(int boardsize, int[][] ladders, int[][] snakes) {
-        Queue<Space> spacesToExplore = new LinkedList<Space>();
+        Queue<Integer> spacesToExplore = new LinkedList<Integer>();
+        boolean[] visited = new boolean[boardsize];
         int curSpace = 1;
+        spacesToExplore.add(curSpace);
 
-        while (!spacesToExplore.isEmpty()) {
-            for (int i = 0; i < ladders.length; i++) {
-                if (curSpace + 6 >= ladders[i][0] && curSpace + 1 >= ladders[i][0]) {
-                    spacesToExplore.add(new Space(ladders[i][0], 0));
+        int[] spaces = new int[boardsize];
+        for (int i = 0; i < boardsize; i++) {
+            for (int[] ladder : ladders) {
+                if (ladder[0] == i + 1) {
+                    spaces[i] = ladder[1];
+                } else {
+                    spaces[i] = i + 1;
                 }
+                System.out.println(ladder[0]);
             }
-            for (int i = 0; i < snakes.length; i++) {
-                if (curSpace + 6 >= snakes[i][0] && curSpace + 1 >= snakes[i][0]) {
-                    spacesToExplore.add(new Space(snakes[i][0], 0));
+
+            for (int[] snake : snakes) {
+                if (snake[0] == i) {
+                    spaces[i] = snake[1];
+                } else {
+                    spaces[i] = i + 1;
                 }
-            }
-
-            curSpace = spacesToExplore.remove().getSpace();
-
-            if (curSpace == boardsize) {
-                return 1;
             }
         }
 
-        return -1;
+        for (int i : spaces) {
+            System.out.println(i);
+        }
 
-//        System.out.print(boardsize);
-//
-//        System.out.println("\n");
-//
-//        for (int i = 0; i < ladders.length; i++) {
-//            for (int j = 0; j < ladders[i].length; j++) {
-//                System.out.print(ladders[i][j] + " ");
+//        while (!spacesToExplore.isEmpty()) {
+//            for (int i = 0; i < ladders.length; i++) {
+//                if (curSpace + 6 >= ladders[i][0] && curSpace + 1 <= ladders[i][0]) {
+//                    spacesToExplore.add();
+//                }
 //            }
-//            System.out.println();
+//            for (int i = 0; i < snakes.length; i++) {
+//                if (curSpace + 6 >= snakes[i][0] && curSpace + 1 <= snakes[i][0]) {
+//                    spacesToExplore.add();
+//                }
+//            }
+//
+//            if (curSpace == boardsize) {
+//                return 1;
+//            }
 //        }
 //
-//        System.out.println("\n");
-//
-//        for (int i = 0; i < snakes.length; i++) {
-//            for (int j = 0; j < snakes[i].length; j++) {
-//                System.out.print(snakes[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+//        return -1;
+
+        System.out.println(boardsize);
+
+        System.out.println("\n");
+
+        for (int i = 0; i < ladders.length; i++) {
+            for (int j = 0; j < ladders[i].length; j++) {
+                System.out.print(ladders[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        for (int i = 0; i < snakes.length; i++) {
+            for (int j = 0; j < snakes[i].length; j++) {
+                System.out.print(snakes[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return -1;
     }
 }
